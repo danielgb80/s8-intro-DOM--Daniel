ba41f1435a8f96a8cecdec30cace333ef9e4f6c2
Daniel GB <danielgb_80@hotmail.com>
2023-09-23 16:20:13 -0600

    Sesión 8 primera parte

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,24 @@
 console.log("Entro index.js");
+
+const btnBlue = document.getElementById("btnBlue");
+
+//btnBlue.addEventListener("click", function(){
+  //  console.log("click en el boton azul");
+//});
+
+function clickBotonAzul(){
+    console.log("click en el boton azul");
+}
+
+function overBotonAzul(){
+    console.log("entro evento over a boton azul");
+}
+
+function changeBackGroundColorPage(color){
+    document.body.style.backgroundColor = color;
+}
+
+btnBlue.addEventListener("click", changeBackGroundColorPage);
+
+
+btnBlue.addEventListener("mouseover", overBotonAzul);
